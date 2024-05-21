ca458f57210d6547975f1e0a7773ef689daf3fd8
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-05-21 17:43:23 +0530

    added: file to define global constants

---
+++ src/constants.ts
@@ -0,0 +1,20 @@
+export enum BackEndRoutes {
+    Register = '/api/v1/auth/register',
+    Login = '/api/v1/auth/login',
+    Logout = '/api/v1/logout'
+}
+
+export enum FrontEndRoutes {
+    Root = '/',
+    Home = '/home',
+    Auth = '/auth',
+    Register = '/register',
+    Write = '/write'
+}
+
+export enum HttpMethods {
+    Get = 'GET',
+    Post = 'POST',
+    Put = 'PUT',
+    Delete = 'DELETE'
+}
