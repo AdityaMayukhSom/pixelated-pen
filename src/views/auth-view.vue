0406e3779bb3d92dca66ab6a95d25d6925d41b63
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-05-21 19:57:49 +0530

    added: auth view with login form and login functionality

---
+++ src/views/auth-view.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { useUserStore } from '@/stores/user-store'
+import { useRouter } from 'vue-router'
+import { ref } from 'vue'
+import { FrontEndRoutes } from '@/constants'
+
+const username = ref<string>('')
+const password = ref<string>('')
+
+const userStore = useUserStore()
+const router = useRouter()
+
+const handleLoginFormSubmission = async (ev: Event) => {
+    try {
+        await userStore.login(username.value, password.value)
+        router.push(FrontEndRoutes.Home)
+    } catch (error) {
+        // somehow handle the error, maybe show invalid input
+    }
+}
+</script>
+
+<template>
+    <h2 class="">Login - Pixelaed Pen</h2>
+    <form @submit.prevent="handleLoginFormSubmission" class="">
+        <div class="w-full gap-y-3 md:w-72 grid grid-flow-row">
+            <label for="username">Username</label>
+            <input type="text" name="username" id="username" v-model.trim="username" />
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password" v-model.trim="password" />
+            <button type="submit">Login</button>
+        </div>
+    </form>
+</template>
