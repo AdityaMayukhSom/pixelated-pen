49de54a9fdab1d038bba8419ca69c0dc077c4d55
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-05-21 19:59:32 +0530

    added: app template for render root

---
+++ src/app.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import FooterComponent from './components/footer-component.vue'
+import HeaderComponent from './components/header-component.vue'
+</script>
+
+<template>
+    <header-component />
+    <router-view />
+    <footer-component />
+</template>
+
+<style scoped></style>
