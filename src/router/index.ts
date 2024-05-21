8e0981a8edc9c52a2b4df110aabc573677f275e1
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-05-21 21:49:10 +0530

    added: nagivation guards for already authenticated and not authenticated routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,18 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/views/home-view.vue'
+import { createRouter, createWebHistory, type RouteLocationNormalized } from 'vue-router'
 import { FrontEndRoutes } from '@/constants'
+import HomeView from '@/views/home-view.vue'
+import { useUserStore } from '@/stores/user-store'
+import pinia from '@/stores'
+
+function alreadyAuthenticated(to: RouteLocationNormalized, from: RouteLocationNormalized) {
+    const userStore = useUserStore(pinia)
+    if (userStore.user.isLoggedIn()) {
+        return {
+            path: FrontEndRoutes.Home
+        } as RouteLocationNormalized
+    }
+    return true
+}
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,7 +22,8 @@ const router = createRouter({
             name: 'home',
             component: HomeView,
             meta: {
-                title: 'Home - Pixelated Pen'
+                title: 'Home - Pixelated Pen',
+                requiresAuth: true
             }
         },
         {
@@ -18,15 +31,18 @@ const router = createRouter({
             name: 'auth',
             component: () => import('@/views/auth-view.vue'),
             meta: {
-                title: 'Auth - Pixelated Pen'
-            }
+                title: 'Auth - Pixelated Pen',
+                requiresAuth: false
+            },
+            beforeEnter: [alreadyAuthenticated]
         },
         {
             path: FrontEndRoutes.Register,
             name: 'register',
             component: () => import('@/views/register-view.vue'),
             meta: {
-                title: 'Register - Pixelated Pen'
+                title: 'Register - Pixelated Pen',
+                requiresAuth: false
             }
         },
         {
@@ -34,13 +50,14 @@ const router = createRouter({
             name: 'write',
             component: () => import('@/views/write-view.vue'),
             meta: {
-                title: 'Write - Pixelated Pen'
+                title: 'Write - Pixelated Pen',
+                requiresAuth: true
             }
         }
     ]
 })
 
-router.beforeEach((to) => {
+router.beforeEach((to, from) => {
     const defaultTitle = 'Pixelated Pen'
     const foundTitle = to.meta.title as string
     document.title = foundTitle || defaultTitle
@@ -48,6 +65,20 @@ router.beforeEach((to) => {
     // const defaultDescription = 'Default Description'
     // const descriptionElement = document.querySelector('head meta[name="description"]')
     // descriptionElement.setAttribute('content', description || defaultDescription)
+
+    const userStore = useUserStore(pinia)
+
+    if (to.meta.requiresAuth && userStore.user.isEmpty()) {
+        // this route requires auth, check if logged in
+        // if not, redirect to login page.
+        return {
+            path: FrontEndRoutes.Auth,
+            // save the location we were at to come back later
+            query: {
+                redirect: to.fullPath
+            }
+        }
+    }
 })
 
 export default router
