482f932e065961020e50b8ecd17714d57353c3aa
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-05-23 12:43:40 +0530

    fixed: variables for backend routes and is-authenticated route

--- src/constants.ts
+++ src/constants.ts
@@ -1,19 +1,22 @@
 // in case of local development, put the url of the backend
 // where api serving code is hosted (eg: http://localhost:8080 etc)
-const BACKEND_BASE_URL = ''
+const BACKEND_BASE_URL = '/api'
+const BACKEND_API_VERSION = '/v1'
+const BACKEND_PREFIX = BACKEND_BASE_URL + BACKEND_API_VERSION
 
 export enum BackEndRoutes {
-    Register = BACKEND_BASE_URL + '/api/v1/auth/register',
-    Login = BACKEND_BASE_URL + '/api/v1/auth/login',
-    Logout = BACKEND_BASE_URL + '/api/v1/logout'
+    Register = BACKEND_PREFIX + `/auth/register`,
+    IsAuthenticated = BACKEND_PREFIX + '/auth/is-authenticated',
+    Login = BACKEND_PREFIX + '/auth/login',
+    Logout = BACKEND_PREFIX + '/logout'
 }
 
 export enum FrontEndRoutes {
     Root = '/',
     Home = '/home',
     Auth = '/auth',
-    Register = '/register',
-    Write = '/write'
+    Write = '/write',
+    Register = '/register'
 }
 
 export enum HttpMethods {
