d8f4905b626e5295d86523b6ee3ef6cf8b318c9e
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-05-23 12:44:03 +0530

    added: zod verification for whether authenticated response

--- src/response.ts
+++ src/response.ts
@@ -7,3 +7,18 @@ export const ZodLoginResponse = z.object({
         username: z.string()
     })
 })
+
+export const ZodAuthenticatedResponse = z.object({
+    type: z.string(),
+    authenticated: z
+        .boolean()
+        .describe(
+            'true if the user is actually authenticated, and must contain a user object, if false, the user object may not be present and the user is not authenticated.'
+        )
+        .readonly()
+        .catch(false),
+    user: z.object({
+        name: z.string(),
+        username: z.string()
+    })
+})
