662d6ebf0635a5fc7e0d63fb339a9604ea776b81
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-10-13 15:13:56 +0530

    Moved project to React.JS

--- src/components/footer-component.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-const year: number = new Date().getFullYear()
-</script>
-
-<template>
-    <footer class="pt-12">Copyright {{ year }} - All Rights Reserved.</footer>
-</template>
-
-<style lang=""></style>
